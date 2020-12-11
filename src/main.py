e56b3f80d90640e0fee364129a73af35adafb80f
Priyanshu Singh <37958393+TheCelestial25@users.noreply.github.com>
2020-12-11 22:47:14 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import os
 import natsort
 import numpy as np
 import datetime
-import cv2
+import cv2 
 from skimage.color import rgb2hsv
-
+import skimage.measure
 
 from color_equalisation import RGB_equalisation
 from global_stretching_RGB import stretching
@@ -25,40 +25,50 @@ if __name__ == '__main__':
     pass
 
 # folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/RayleighDistribution"
-folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
-
-path = folder + "/InputImages"
+folder = "D:/Single-Underwater-Image-Enhancement-and-Color-Restoration-master/underwater-test-dataset/upload/DATABASE 1"
+path = folder 
 files = os.listdir(path)
 files =  natsort.natsorted(files)
+starttime = datetime.datetime.now()
+c =1
+avg_psnr=0
+avg_mse=0
+avg_entropy=0
+for i in files:    
+    img = cv2.cv2.imread("D:/Single-Underwater-Image-Enhancement-and-Color-Restoration-master/underwater-test-dataset/upload/DATABASE 1./"+str(c)+".jpg")
+    height = len(img)
+    width = len(img[0])
+    sceneRadiance = RGB_equalisation(img, height, width)
+    # sceneRadiance = stretching(img)
+    sceneRadiance = stretching(sceneRadiance)
+    sceneRadiance_Lower, sceneRadiance_Upper = rayleighStretching(sceneRadiance, height, width)
 
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        # img = cv2.imread('InputImages/' + file)
-        img = cv2.imread(folder + '/InputImages/' + file)
-        prefix = file.split('.')[0]
-        height = len(img)
-        width = len(img[0])
+    sceneRadiance = (np.float64(sceneRadiance_Lower) + np.float64(sceneRadiance_Upper)) / 2
 
-        sceneRadiance = RGB_equalisation(img, height, width)
-        # sceneRadiance = stretching(img)
-        sceneRadiance = stretching(sceneRadiance)
-        sceneRadiance_Lower, sceneRadiance_Upper = rayleighStretching(sceneRadiance, height, width)
+    # cv2.imwrite('OutputImages/' + prefix + 'Lower0.jpg', sceneRadiance_Lower)
+    # cv2.imwrite('OutputImages/' + prefix + 'Upper0.jpg', sceneRadiance_Upper)
 
-        sceneRadiance = (np.float64(sceneRadiance_Lower) + np.float64(sceneRadiance_Upper)) / 2
-
-        # cv2.imwrite('OutputImages/' + prefix + 'Lower0.jpg', sceneRadiance_Lower)
-        # cv2.imwrite('OutputImages/' + prefix + 'Upper0.jpg', sceneRadiance_Upper)
-
-        sceneRadiance = HSVStretching(sceneRadiance)
-        sceneRadiance = sceneRadianceRGB(sceneRadiance)
-        cv2.imwrite('OutputImages/' + prefix + '_RayleighDistribution.jpg', sceneRadiance)
-
+    sceneRadiance = HSVStretching(sceneRadiance)
+    sceneRadiance = sceneRadianceRGB(sceneRadiance)
+    cv2.cv2.imwrite( str(c)+'_out.jpg', sceneRadiance)
+    entropy = skimage.measure.shannon_entropy(sceneRadiance)
+    print(str(entropy) + '\t')
+    mse = skimage.measure.compare_mse(img,sceneRadiance)
+    
+    psnr = skimage.measure.compare_psnr(img, sceneRadiance)
+    print(str(mse) + '\t' + str(psnr)+'\n')
+    c=c+1
+    avg_entropy+=entropy
+    avg_psnr+=psnr
+    avg_mse+=mse 
 
 
 endtime = datetime.datetime.now()
 time = endtime-starttime
-print('time',time)
+print('time := ',time )
+print( '\n' )
+print('avg_psnr := ', avg_psnr/len(files))
+print('\n')
+print('avg_mse := ',avg_mse/len(files))
+print('\n')
+print('avg_entropy := ', avg_entropy/len(files))
